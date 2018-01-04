2a612b045143fad6ee05385e6ed7a0e662eb8dfa
Jay Geiss <jaygeiss@umich.edu>
2018-01-03 20:26:05 -0500

    initial commit

---
+++ Project 1/Project 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 1
+//
+//  Created by Jay Geiss on 1/3/18.
+//  Copyright © 2018 Jay Geiss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
